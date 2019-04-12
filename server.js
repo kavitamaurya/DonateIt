c06236b41249d93b007d085c0a8d5045e414c7a4
kavita-maurya <kavita.maurya@inmobi.com>
2019-04-12 23:06:00 +0530

    server include all

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-// viewed at http://localhost:8080
-app.get('/', function(req, res) {
-    res.sendfile(path.join(__dirname + '/index.html'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/images', express.static(__dirname + '/images'));
+app.use('/bootstrap-4.0.0-dist', express.static(__dirname + '/bootstrap-4.0.0-dist'));
+app.use('/vendor', express.static(__dirname + '/vendor'));
+app.use('/build', express.static(__dirname + '/build'));
+app.use('/HackathonDashboard', express.static(__dirname + '/HackathonDashboard'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
+app.all('/*', function(req, res, next) {
+    // Just send the index.html for other files to support HTML5Mode
+    res.sendfile('index.html', { root: __dirname });
 });
 
 app.listen(8080);
